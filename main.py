91900db3c79c909146d4e88f3967d606717d9dae
jacobszpz <jacobszpz@protonmail.com>
2023-05-20 01:07:30 +0100

    Create entrypoint for pyinstaller

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+BlackboardSync python entrypoint
+"""
+
+# Copyright (C) 2021, Jacob Sánchez Pérez
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from blackboard_sync.sync_controller import BBSyncController
+
+
+def main() -> int:
+    BBSyncController()
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
