63c418054d1881b4d3caa0519bd5c4d604eec673
jacobszpz <jacobszpz@protonmail.com>
2023-06-19 12:57:12 +0100

    Release 0.8.5

--- blackboard_sync/__about__.py
+++ blackboard_sync/__about__.py
@@ -33,7 +33,7 @@ __title__ = "BlackboardSync"
 __summary__ = "Automatic Syncing Of Your Blackboard Content"
 __uri__ = "https://github.com/jacobszpz/BlackboardSync"
 
-__version__ = "0.8.5-rc.4"
+__version__ = "0.8.5"
 
 __author__ = "Jacob Sánchez"
 __email__ = "jacobszpz@protonmail.com"
