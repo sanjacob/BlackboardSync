8cb7f9b128174e16c5ca5857868f5a43de38773c
Jacob Sánchez <jacobszpz@protonmail.com>
2023-09-26 12:06:46 +0100

    Fix reqs needed for documentation build

--- docs/conf.py
+++ docs/conf.py
@@ -49,6 +49,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+autoclass_content = 'both'
 source_suffix = {
     '.rst': 'restructuredtext',
     '.md': 'markdown',
