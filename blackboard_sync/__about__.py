0fd8ae827d42c472936c47edff3dd2563b44496f
Jacob Sánchez <jacobszpz@protonmail.com>
2023-09-29 16:49:47 +0100

    Add .desktop and .metainfo files for linux packaging

--- blackboard_sync/__about__.py
+++ blackboard_sync/__about__.py
@@ -33,7 +33,7 @@ __title__ = "BlackboardSync"
 __summary__ = "Automatic Syncing Of Your Blackboard Content"
 __uri__ = "https://github.com/jacobszpz/BlackboardSync"
 
-__version__ = "0.9.12-alpha.1"
+__version__ = "0.9.12-alpha.1.1"
 
 __author__ = "Jacob Sánchez"
 __email__ = "jacobszpz@protonmail.com"
