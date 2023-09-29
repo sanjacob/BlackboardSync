6c0c5f14df852c79314a90dcd16f30e1e0d303b8
Jacob Sánchez <jacobszpz@protonmail.com>
2023-09-29 14:43:49 +0100

    Change flatpak workflow to build blackboardsync from github not pypi

--- blackboard_sync/__about__.py
+++ blackboard_sync/__about__.py
@@ -33,7 +33,7 @@ __title__ = "BlackboardSync"
 __summary__ = "Automatic Syncing Of Your Blackboard Content"
 __uri__ = "https://github.com/jacobszpz/BlackboardSync"
 
-__version__ = "0.9.12-beta.20"
+__version__ = "0.9.12-beta.21"
 
 __author__ = "Jacob Sánchez"
 __email__ = "jacobszpz@protonmail.com"
