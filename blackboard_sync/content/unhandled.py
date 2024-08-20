995b1844cd34cfe2071420a72e28646b122807bd
Jacob Sánchez <jacobszpz@protonmail.com>
2024-08-20 08:12:16 +0100

    Add logging statement to unhandled content

--- blackboard_sync/content/unhandled.py
+++ blackboard_sync/content/unhandled.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 from concurrent.futures import ThreadPoolExecutor
 
@@ -6,12 +7,14 @@ from blackboard.blackboard import BBCourseContent
 
 from .job import DownloadJob
 
+logger = logging.getLogger(__name__)
+
 
 class Unhandled:
     """Content which we cannot handle yet, or at all."""
 
     def __init__(self, content: BBCourseContent, _, job: DownloadJob):
-        pass
+        logger.info(f"{content.title} not supported")
 
     def write(self, path: Path, executor: ThreadPoolExecutor):
         pass
