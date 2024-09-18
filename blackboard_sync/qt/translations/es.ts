1192c0745080ec079202cf44a15ec34a89b5c5af
Jacob Sánchez <jacobszpz@protonmail.com>
2024-09-18 12:23:32 +0100

    Add Spanish (es) translation (#374)

---
+++ blackboard_sync/qt/translations/es.ts
@@ -0,0 +1,345 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es" sourcelanguage="en">
+<context>
+    <name>Dialogs</name>
+    <message>
+        <location filename="../dialogs.py" line="45"/>
+        <source>Do you wish to redownload all files?</source>
+        <translation>¿Volver a descargar los documentos?</translation>
+    </message>
+    <message>
+        <location filename="../dialogs.py" line="46"/>
+        <source>Answer no if you intend to move all past downloads manually (Recommended).</source>
+        <translation>Si deseas mover tus descargas previas manualmente, presiona No (recomendado).</translation>
+    </message>
+    <message>
+        <location filename="../dialogs.py" line="60"/>
+        <source>Unfortunately, your university is not yet supported</source>
+        <translation>Desafortunadamente, tu universidad aún no es compatible</translation>
+    </message>
+    <message>
+        <location filename="../dialogs.py" line="63"/>
+        <source>You can help us provide support for it by visiting our website, which you can access by pressing the help button.</source>
+        <translation>Nos puedes ayudar a agregar tu institución a la aplicación, lo cuál puedes hacer al presionar el botón de ayuda.</translation>
+    </message>
+    <message>
+        <location filename="../dialogs.py" line="78"/>
+        <source>There was an issue logging you in</source>
+        <translation>Hubo un problema al ingresar</translation>
+    </message>
+    <message>
+        <location filename="../dialogs.py" line="81"/>
+        <source>Please try again later, and if the error persists contact our support by pressing the button below.</source>
+        <translation>Por favor intenta más tarde, o solicita ayuda presionando el botón debajo.</translation>
+    </message>
+</context>
+<context>
+    <name>LoginWebView</name>
+    <message>
+        <location filename="../LoginWebView.ui" line="0"/>
+        <source>Log in to Blackboard Learn</source>
+        <translation>Inicia sesión en Blackboard Learn</translation>
+    </message>
+    <message>
+        <location filename="../LoginWebView.ui" line="0"/>
+        <source>This window will close once you log in.</source>
+        <translation>Esta ventana se cerrará una vez que ingreses.</translation>
+    </message>
+    <message>
+        <location filename="../LoginWebView.ui" line="0"/>
+        <source>Go back</source>
+        <translation>Página previa</translation>
+    </message>
+    <message>
+        <location filename="../LoginWebView.ui" line="0"/>
+        <source>Go to start</source>
+        <translation>Volver al inicio</translation>
+    </message>
+    <message>
+        <location filename="../LoginWebView.ui" line="0"/>
+        <source>Report issue</source>
+        <translation>Reportar problema</translation>
+    </message>
+    <message>
+        <location filename="../LoginWebView.py" line="125"/>
+        <source>Trouble logging in? Press the help button to let us know.</source>
+        <translation>¿Problemas al ingresar? Presiona el botón de ayuda para informarnos.</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWindow</name>
+    <message>
+        <location filename="../SettingsWindow.ui" line="0"/>
+        <source>Settings</source>
+        <translation>Preferencias</translation>
+    </message>
+    <message>
+        <location filename="../SettingsWindow.ui" line="0"/>
+        <source>Download location</source>
+        <translation>Ubicación de descarga</translation>
+    </message>
+    <message>
+        <location filename="../SettingsWindow.ui" line="0"/>
+        <source>Location to be shown here</source>
+        <translation>Ubicación se mostrará aquí</translation>
+    </message>
+    <message>
+        <location filename="../SettingsWindow.ui" line="0"/>
+        <source>Change location</source>
+        <translation>Cambiar ubicación</translation>
+    </message>
+    <message>
+        <location filename="../SettingsWindow.ui" line="0"/>
+        <source>Sync every</source>
+        <translation>Descargar cada</translation>
+    </message>
+    <message>
+        <location filename="../SettingsWindow.ui" line="0"/>
+        <source>30 minutes</source>
+        <translation>30 minutos</translation>
+    </message>
+    <message>
+        <location filename="../SettingsWindow.ui" line="0"/>
+        <source>1 hour</source>
+        <translation>1 hora</translation>
+    </message>
+    <message>
+        <location filename="../SettingsWindow.ui" line="0"/>
+        <source>6 hours</source>
+        <translation>6 horas</translation>
+    </message>
+    <message>
+        <location filename="../SettingsWindow.ui" line="0"/>
+        <source>User session</source>
+        <translation>Sesión de usuario</translation>
+    </message>
+    <message>
+        <location filename="../SettingsWindow.ui" line="0"/>
+        <source>Logged in as</source>
+        <translation>Ingresaste como</translation>
+    </message>
+    <message>
+        <location filename="../SettingsWindow.ui" line="0"/>
+        <source>Log out</source>
+        <translation>Cerrar sesión</translation>
+    </message>
+    <message>
+        <location filename="../SettingsWindow.ui" line="0"/>
+        <source>Initial setup</source>
+        <translation>Configuración</translation>
+    </message>
+    <message>
+        <location filename="../SettingsWindow.py" line="107"/>
+        <source>Logged in as </source>
+        <translation>Ingresaste como </translation>
+    </message>
+    <message>
+        <location filename="../SettingsWindow.py" line="110"/>
+        <source>Not currently logged in</source>
+        <translation>No has ingresado</translation>
+    </message>
+</context>
+<context>
+    <name>SetupWizard</name>
+    <message>
+        <location filename="../SetupWizard.ui" line="0"/>
+        <source>Setup</source>
+        <translation>Configuración</translation>
+    </message>
+    <message>
+        <location filename="../SetupWizard.ui" line="0"/>
+        <source>You are a few steps away from syncing your Blackboard Learn content straight to your device!</source>
+        <translation>¡Estás a unos pasos de descargar tu contenido de Blackboard Learn a tu dispositivo!</translation>
+    </message>
+    <message>
+        <location filename="../SetupWizard.ui" line="0"/>
+        <source>First, tell us where you study</source>
+        <translation>Primero, dime dónde estudias</translation>
+    </message>
+    <message>
+        <location filename="../SetupWizard.ui" line="0"/>
+        <source>University auto-detected by internet service provider</source>
+        <translation>Universidad auto-detectada por proveedor de internet</translation>
+    </message>
+    <message>
+        <location filename="../SetupWizard.ui" line="0"/>
+        <source>Where do you want files to be downloaded?</source>
+        <translation>¿A dónde quieres que se descarguen los documentos?</translation>
+    </message>
+    <message>
+        <location filename="../SetupWizard.ui" line="0"/>
+        <source>Select...</source>
+        <translation>Seleccionar...</translation>
+    </message>
+    <message>
+        <location filename="../SetupWizard.ui" line="0"/>
+        <source>Download courses starting from...</source>
+        <translation>Descargar cursos empezando desde...</translation>
+    </message>
+    <message>
+        <location filename="../SetupWizard.ui" line="0"/>
+        <source>Download all content</source>
+        <translation>Descarga todo el contenido</translation>
+    </message>
+</context>
+<context>
+    <name>SyncTrayMenu</name>
+    <message>
+        <location filename="../SyncTrayIcon.py" line="45"/>
+        <source>Sync now</source>
+        <translation>Descargar ahora</translation>
+    </message>
+    <message>
+        <location filename="../SyncTrayIcon.py" line="49"/>
+        <source>Open downloads</source>
+        <translation>Abrir descargas</translation>
+    </message>
+    <message>
+        <location filename="../SyncTrayIcon.py" line="53"/>
+        <source>Preferences</source>
+        <translation>Preferencias</translation>
+    </message>
+    <message>
+        <location filename="../SyncTrayIcon.py" line="58"/>
+        <source>You haven&apos;t logged in</source>
+        <translation>No has ingresado</translation>
+    </message>
+    <message>
+        <location filename="../SyncTrayIcon.py" line="62"/>
+        <source>Setup</source>
+        <translation>Configuración</translation>
+    </message>
+    <message>
+        <location filename="../SyncTrayIcon.py" line="65"/>
+        <source>Quit</source>
+        <translation>Salir</translation>
+    </message>
+    <message>
+        <location filename="../SyncTrayIcon.py" line="78"/>
+        <source>Not logged in</source>
+        <translation>No has ingresado</translation>
+    </message>
+    <message>
+        <location filename="../SyncTrayIcon.py" line="83"/>
+        <source>Last synced: </source>
+        <translation>Última descarga: </translation>
+    </message>
+    <message>
+        <location filename="../SyncTrayIcon.py" line="89"/>
+        <source>Downloading now...</source>
+        <translation>Descargando...</translation>
+    </message>
+</context>
+<context>
+    <name>TimeStrings</name>
+    <message>
+        <location filename="../utils.py" line="87"/>
+        <source>second</source>
+        <translation>segundo</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="89"/>
+        <source>minute</source>
+        <translation>minuto</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="91"/>
+        <source>hour</source>
+        <translation>hora</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="93"/>
+        <source>day</source>
+        <translation>dia</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="95"/>
+        <source>week</source>
+        <translation>semana</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="97"/>
+        <source>month</source>
+        <translation>mes</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="99"/>
+        <source>year</source>
+        <translation>año</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="104"/>
+        <source>seconds</source>
+        <translation>segundos</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="106"/>
+        <source>minutes</source>
+        <translation>minutos</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="108"/>
+        <source>hours</source>
+        <translation>horas</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="110"/>
+        <source>days</source>
+        <translation>días</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="112"/>
+        <source>weeks</source>
+        <translation>semanas</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="114"/>
+        <source>months</source>
+        <translation>meses</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="116"/>
+        <source>years</source>
+        <translation>años</translation>
+    </message>
+    <message>
+        <location filename="../utils.py" line="120"/>
+        <source>{n} {name} ago</source>
+        <translation>Hace {n} {name}</translation>
+    </message>
+</context>
+<context>
+    <name>TrayMessages</name>
+    <message>
+        <location filename="../notification.py" line="56"/>
+        <source>An update is available</source>
+        <translation>Una actualización está disponible</translation>
+    </message>
+    <message>
+        <location filename="../notification.py" line="57"/>
+        <source>You can update the app from your digital store</source>
+        <translation>Puedes actualizar la app desde tu tienda digital</translation>
+    </message>
+    <message>
+        <location filename="../notification.py" line="61"/>
+        <source>The download could not be completed</source>
+        <translation>La descarga no se pudo completar</translation>
+    </message>
+    <message>
+        <location filename="../notification.py" line="62"/>
+        <source>There was an error while downloading your content</source>
+        <translation>Hubo un error al descargar tu contenido</translation>
+    </message>
+    <message>
+        <location filename="../notification.py" line="66"/>
+        <source>The app is running in the background</source>
+        <translation>La app está corriendo en el fondo</translation>
+    </message>
+    <message>
+        <location filename="../notification.py" line="67"/>
+        <source>Click the tray icon to manage your downloads</source>
+        <translation>Puedes gestionar tus descargas desde la bandeja</translation>
+    </message>
+</context>
+</TS>
